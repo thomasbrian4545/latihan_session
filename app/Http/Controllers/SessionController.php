b855da3ba43e2b69251ae2681614f721bdcb3224
thomasbrian4545 <thomasbrian4545@gmail.com>
2023-09-25 00:02:38 +0700

    Membuat controller SessionController.php
    - Mengisi route

---
+++ app/Http/Controllers/SessionController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SessionController extends Controller
+{
+    public function index()
+    {
+        echo '<ul>';
+        echo '<li><a href=/buat-session>Buat Session</a></li>';
+        echo '<li><a href=/akses-session>Akses Session</a></li>';
+        echo '<li><a href=/hapus-session>Hapus Session</a></li>';
+        echo '<li><a href=/flash-session>Flash Session</a></li>';
+        echo '</ul>';
+    }
+    public function buatSession()
+    {
+        //
+    }
+    public function aksesSession()
+    {
+        //
+    }
+    public function hapusSession()
+    {
+        //
+    }
+    public function flashSession()
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SessionController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [SessionController::class, 'index']);
+Route::get('/buat-session', [SessionController::class, 'buatSession']);
+Route::get('/akses-session', [SessionController::class, 'aksesSession']);
+Route::get('/hapus-session', [SessionController::class, 'hapusSession']);
+Route::get('/flash-session', [SessionController::class, 'flashSession']);
